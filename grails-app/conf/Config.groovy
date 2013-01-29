ce07ce3199e5671d13a0eb2a525414f96fa67715
Heikki Verta <heikki@applifier.com>
2013-01-29 10:18:32 +0100

    New server url http://spede.dyndns.org:8080

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -47,7 +47,7 @@ grails.spring.bean.packages = []
 // set per-environment serverURL stem for creating absolute links
 environments {
     production {
-        grails.serverURL = "http://pertti.dyndns.info/${appName}"
+        grails.serverURL = "http://spede.dyndns.org:8080/${appName}"
     }
     development {
         grails.serverURL = "http://localhost:8080/${appName}"
